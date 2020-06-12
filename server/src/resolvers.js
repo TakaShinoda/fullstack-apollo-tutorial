6391c4ed2a7a14cd25831d13ed06ddcc02ca0f0d
Takahiro <tkhr_621@docomo.ne.jp>
2020-06-12 23:34:55 +0900

    Add: resolver.jsを追加

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -7,3 +7,10 @@ module.exports = {
       me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
     }
   };
+
+
+// fieldName: (parent, args, context, info) => data;
+// parent: 親 resolver から受け取ったオブジェクト。
+// args: この field に対して渡された引数。
+// context: GraphQL operation の resolver 全体で共有されるオブジェクト。このチュートリアルでは認証情報やデータソースを context で共有している。
+// info: 実行したオペレーションに関する状態等の詳細情報。通常は用いられないがよりアドバンスなケースにおいて使われることが多い。
