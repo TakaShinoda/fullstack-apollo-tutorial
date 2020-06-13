c8d6f180fe0e21481187048dff3c04a724060607
Takahiro <tkhr_621@docomo.ne.jp>
2020-06-13 21:48:10 +0900

    Fix: GraphQLのresolverを作成

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,35 +1,49 @@
 const { gql } = require('apollo-server')
 
 const typeDefs = gql`
-  # ここにスキーマを書きます
-
   type Query {
-    launches: launches( # replace the current launches query with this one.
-    """
-    The number of results to show. Must be >= 1. Default = 20
-    """
-    pageSize: Int
-    """
-    If you add a cursor here, it will only return results _after_ this cursor
-    """
-    after: String
-  ): LaunchConnection!
-  launch(id: ID!): Launch
-  me: User
-}
+    launches(
+      """
+      The number of results to show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+      """
+      If you add a cursor here, it will only return results _after_ this cursor
+      """
+      after: String
+    ): LaunchConnection!
+    launch(id: ID!): Launch
+    me: User
+  }
 
-"""
-Simple wrapper around our list of launches that contains a cursor to the
-last item in the list. Pass this cursor to the launches query to fetch results
-after these.
-"""
-type LaunchConnection { # add this below the Query type as an additional type.
-  cursor: String!
-  hasMore: Boolean!
-  launches: [Launch]!
-}
+  type Mutation {
+    # if false, signup failed -- check errors
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
 
+    # if false, cancellation failed -- check errors
+    cancelTrip(launchId: ID!): TripUpdateResponse!
 
+    login(email: String): String # login token
+    # for use with the iOS tutorial
+    uploadProfileImage(file: Upload!): User
+  }
+
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
+
+  """
+  Simple wrapper around our list of launches that contains a cursor to the
+  last item in the list. Pass this cursor to the launches query to fetch results
+  after these.
+  """
+  type LaunchConnection {
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
+  }
 
   type Launch {
     id: ID!
@@ -48,33 +62,23 @@ type LaunchConnection { # add this below the Query type as an additional type.
   type User {
     id: ID!
     email: String!
+    profileImage: String
     trips: [Launch]!
   }
 
+  # type Mission {
+  #   name: String
+  #   missionPatch(size: PatchSize): String
+  # }
+
   type Mission {
-    name: String
-    missionPatch(size: PatchSize): String
+    # ... with rest of schema
+    missionPatch(mission: String, size: PatchSize): String
   }
 
   enum PatchSize {
     SMALL
     LARGE
-  }
-
-  type Mutation {
-    # if false, booking trips failed -- check errors
-    bookTrips(launchIds: [ID]!): TripUpdateResponse!
-
-    # if false, cancellation failed -- check errors
-    cancelTrip(launchId: ID!): TripUpdateResponse!
-
-    login(email: String): String # login token
-  }
-
-  type TripUpdateResponse {
-    success: Boolean!
-    message: String
-    launches: [Launch]
   }
 `
 
